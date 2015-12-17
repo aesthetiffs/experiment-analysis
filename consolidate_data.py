1ac42799b6cda29d28bc2d03b5cd21ccbc929543
aesthetiffs <tiffani.ng@gmail.com>
2015-12-16 21:31:41 -0600

    Outputs CSV of consolidated data.

--- consolidate_data.py
+++ consolidate_data.py
@@ -6,8 +6,18 @@ import re
 
 # Config - change these if needful!
 datafile_dir = 'data'
+output_dir = 'processed_data'
 extract_columns = ['cra', 'CRAsolution', 'solution_cra.keys', 'solution_cra.rt', 'cra_solved.keys', 'cra_solved.rt', 'insight_resp.keys', 'insight_resp.rt', 'date', 'frameRate', 'expName', 'participant', 'condition']
 # End config
+
+def mangleFile(orig_filename, csv_writer):
+  with open(orig_filename) as csv_fd:
+    csv_rows = csv.DictReader(csv_fd)
+    for row in csv_rows:
+      # new_csv.append([row[col_name] for col_name in extract_columns])
+      csv_writer.writerow([row[col_name] for col_name in extract_columns])
+
+  return True
 
 datafile_list = glob.glob(os.path.join('.', datafile_dir, '*.csv'))
 file_pairs = {}
@@ -31,16 +41,13 @@ for file_name in datafile_list:
 # The pre and post files are different, so load the CSV and pull out the
 # relevant columns, leaving the rest of the junk columns behind.
 for participant in file_pairs:
-  print "* Participant %s" % (participant)
+  print "* Participant %s processing..." % (participant)
+  output_filename = '%s_all.csv' % (participant)
+  output_path = os.path.join('.', output_dir, output_filename)
 
-  for exp in file_pairs[participant]:
-    file_name = file_pairs[participant][exp]
-    new_csv = []
+  with open(output_path, 'wb') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(extract_columns)
     
-    with open(file_name) as csv_fd:
-      csv_rows = csv.DictReader(csv_fd)
-      for row in csv_rows:
-        new_csv.append([row[col_name] for col_name in extract_columns])
-
-    print new_csv
-    print "\n ======= \n"
+    mangleFile(file_pairs[participant]['pre'], csv_writer)
+    mangleFile(file_pairs[participant]['post'], csv_writer)
